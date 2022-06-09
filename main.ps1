3e490920a4ddca754417750f10a7690517adf3a9
Павло Онищенко <pbl6a@live.com>
2022-06-09 17:38:32 +0300

    +

--- main.ps1
+++ main.ps1
@@ -20,10 +20,52 @@ if ($FreeMem -lt $RamLimit) {
 }
 $RunnerURL = $XMLConfig.config.links.runner;
 Get-File $RunnerURL $("$RootDir\\runner.ps1")
+$NewStartRequired = $true;
+$TitleStarted = $XMLConfig.config.titles.started;
+$TitleOK = $XMLConfig.config.titles.ok;
+$TitleRestart = $XMLConfig.config.titles.restart;
+$TitleError = $XMLConfig.config.titles.error;
+$TitleDied = $XMLConfig.config.titles.died;
+$TitleExiting = $XMLConfig.config.titles.exiting;
+$TitleCompleted = $XMLConfig.config.titles.completed;
 while ($true) {
+    if ($NewStartRequired) {
+        if ($LiteMode) {
+            $RunnerProc = Start-Process -FilePath $PwshExe -ArgumentList "$RootDir\\runner.ps1 -lite" -NoNewWindow -PassThru -WorkingDirectory $RootDir;
+        }
+        else {
+            $RunnerProc = Start-Process -FilePath $PwshExe -ArgumentList "$RootDir\\runner.ps1" -NoNewWindow -PassThru -WorkingDirectory $RootDir;
+        }
+    }
+    while ($null -eq $RunnerProc.Id) {
+        Start-Sleep -Seconds 1;
+    }
     $Now = [System.DateTime]::Now;
-    if ($LiteMode) {
-        $RunnerProc = Start-Process -FilePath $PwshExe -ArgumentList "$RootDir\\runner.ps1" -NoNewWindow
+    $NextCheck = $Now.AddMinutes($UpdateCheckTime);
+    while ($Now -lt $NextCheck -and -not $NewStartRequired) {
+        if ($RunnerProc.HasExited) {
+            $NewStartRequired = $true;
+        }
+        $testRunnerProc = Get-Process -Id $RunnerProc.Id;
+        if (-not $testRunnerProc) {
+            $NewStartRequired = $true;
+        }
+    }
+    if ($NewStartRequired) {
+        Stop-Tree $RunnerProc.Id;
+    }
+    Get-File $RunnerURL $("$RootDir\\runner_new.ps1");
+    $Now = [System.DateTime]::Now;
+    $File = Get-Item $("$RootDir\\runner.ps1");
+    $File.LastWriteTime = $Now;
+    $File = Get-Item $("$RootDir\\runner_new.ps1");
+    $File.LastWriteTime = $Now;
+    $GotNewVersion = FilesAreEqual $("$RootDir\\runner.ps1") $("$RootDir\\runner_new.ps1");
+    if ($GotNewVersion) {
+        $NewStartRequired = $true;
+        Stop-Tree $RunnerProc.Id;
+        Remove-Item -Path $("$RootDir\\runner.ps1") -Force;
+        Rename-Item -Path $("$RootDir\\runner_new.ps1") -NewName $("$RootDir\\runner.ps1");
     }
     
 }
