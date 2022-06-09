fddc6fe1cea19efe8e42c7a01d105068c1ab5888
Павло Онищенко <pbl6a@live.com>
2022-06-09 16:58:02 +0300

    +

--- main.ps1
+++ main.ps1
@@ -1,3 +1,4 @@
+# DO REMEMBER CTRL+C breaks the pipeline!
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls";
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("UTF-8");
 # WebClient is outdated. Use HttpClient instead.
@@ -16,4 +17,13 @@ $RamLimit = $XMLConfig.config.limits.RAM
 $LiteMode = $false
 if ($FreeMem -lt $RamLimit) {
     $LiteMode = $true;
+}
+$RunnerURL = $XMLConfig.config.links.runner;
+Get-File $RunnerURL $("$RootDir\\runner.ps1")
+while ($true) {
+    $Now = [System.DateTime]::Now;
+    if ($LiteMode) {
+        $RunnerProc = Start-Process -FilePath $PwshExe -ArgumentList "$RootDir\\runner.ps1" -NoNewWindow
+    }
+    
 }
