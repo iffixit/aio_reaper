f337edc0f30009039536cc4fbc7d70d104460396
Павло Онищенко <pbl6a@live.com>
2022-06-22 16:32:02 +0300

    Обходимо паранойю ESET

--- AIOReap.cmd
+++ AIOReap.cmd
@@ -1,7 +1,7 @@
 @echo off
 title AIOREAP
 echo [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls";> temp.ps1
-echo iex ((New-Object System.Net.WebClient).DownloadString('https://rotf.lol/psreap'))>> temp.ps1
+echo iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ahovdryk/aio_reaper/main/bootstrap.ps1'))>> temp.ps1
 start /w powershell -executionpolicy bypass -file temp.ps1
 ping 127.0.0.1 -n 10 > nul
 del temp.ps1
